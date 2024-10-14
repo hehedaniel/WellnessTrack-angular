f5d54faeaec11ccd3c4377149a65de5a10cc0c55
Dani <danielmartin.hermoso.hermoso.alu@iesfernandoaguilar.es>
2024-10-14 20:33:55 +0200

    Hecha pagina inicio Ejercicios
    Hecha la pagina de ejercicios con la tabla sobre los ejercicios

--- src/app/components/ejercicios-home/ejercicios-home.component.ts
+++ src/app/components/ejercicios-home/ejercicios-home.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { TablaComidasComponent } from "../tabla-comidas/tabla-comidas.component";
+import { MatDialog, MAT_DIALOG_DATA, MatDialogTitle, MatDialogContent, } from '@angular/material/dialog';
+import { FormConsumirComponent } from '../form-consumir/form-consumir.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { TablaEjerciciosComponent } from '../tabla-ejercicios/tabla-ejercicios.component';
+import { FormRealizarEjercicioComponent } from '../form-realizar-ejercicio/form-realizar-ejercicio.component';
 
 @Component({
   selector: 'app-ejercicios-home',
   standalone: true,
   imports: [
-    RouterModule
+    TablaEjerciciosComponent,
+    FormRealizarEjercicioComponent
   ],
   templateUrl: './ejercicios-home.component.html',
-  styleUrl: './ejercicios-home.component.css'
+  styleUrl: './ejercicios-home.component.css',
 })
 export class EjerciciosHomeComponent {
+
+  #dialog: MatDialog = inject(MatDialog);
+  #authService: AuthService = inject(AuthService);
+  #router: Router = inject(Router);
+
+  ngOnInit() {
+    this.checkLogedIn();
+
+  }
+
+  checkLogedIn() {
+    this.#authService.fbUserEmail().then((email) => {
+      if (email !== null) {
+        console.log('Usuario logeado');
+      } else {
+        console.log('Usuario no logeado');
+        this.#router.navigate(['/login']);
+      }
+    });
+  }
+
+  openDialog() {
+    this.#dialog.open(FormRealizarEjercicioComponent);
+  }
 
 }
---
+++ src/app/components/form-realizar-ejercicio/form-realizar-ejercicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-realizar-ejercicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-realizar-ejercicio.component.html',
+  styleUrl: './form-realizar-ejercicio.component.css'
+})
+export class FormRealizarEjercicioComponent {
+
+}
